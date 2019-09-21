80bac75070d5687726a4e2da65e5d9c7242bf114
dereklitao <dereklitao@outlook.com>
2019-09-22 00:38:52 +0800

    add fan_speed_func

--- typedefine/cpsmodetype.m
+++
@@ -1,8 +0,0 @@
-classdef cpsmodetype < Simulink.IntEnumType
-  enumeration
-    Auto(0)
-    Cold(1)
-    Dehumi(2)
-    Hot(4)
-  end
-end 
--- typedefine/usermodetype.m
+++
@@ -1,9 +0,0 @@
-classdef usermodetype < Simulink.IntEnumType
-  enumeration
-    User_off(0)
-    User_auto(1)
-    User_hot(2)
-    User_dehumi(3)
-    User_venti(4)
-  end
-end 
