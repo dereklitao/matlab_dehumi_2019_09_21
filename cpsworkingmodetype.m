3dfefcab2d28db284a8753cbb4f0ab4bb5fb2040
dereklitao <dereklitao@outlook.com>
2019-09-21 19:40:51 +0800

    add type

---
+++ cpsworkingmodetype.m
@@ -0,0 +1,8 @@
+classdef cpsworkingmodetype < Simulink.IntEnumType
+  enumeration
+    Auto(0)
+    Cold(1)
+    Dehumi(2)
+    Hot(4)
+  end
+end 
---
+++ usermodetype.m
@@ -0,0 +1,9 @@
+classdef usermodetype < Simulink.IntEnumType
+  enumeration
+    User_off(0)
+    User_auto(1)
+    User_hot(2)
+    User_dehumi(3)
+    User_venti(4)
+  end
+end 
